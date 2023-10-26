52ec255acf5cb6c954be17507d174b901256f7e6
BT_2N2_11 <sample@bths.edu>
2023-10-26 14:05:57 -0400

    done

--- src/Main5.java
+++ src/Main5.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main5 {
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive even number less than 100: ");
+        int num = myScanner.nextInt();
+
+        while (num < 0 || num % 2 == 1 || num >= 100) {
+            System.out.println("No! I said a positive even number less than 100!");
+            System.out.print("Try again: ");
+            num = myScanner.nextInt();
+        }
+
+        System.out.println("See, that wasn't so hard!");
+        myScanner.close();
+    }
 }
+
